813e82183c2aa0da08977fb44d19d18d90331b06
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-12-07 19:00:06 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,9 +1,6 @@
 from pyrogram import Client, filters
-from info import CHANNELS, DELETE_CHANNEL
+from info import CHANNELS
 from database.ia_filterdb import save_file, Media, unpack_new_file_id
-import logging
-
-logger = logging.getLogger(__name__)
 
 media_filter = filters.document | filters.video | filters.audio
 
@@ -21,46 +18,3 @@ async def media(bot, message):
     media.file_type = file_type
     media.caption = message.caption
     await save_file(media)
-
-@Client.on_message(filters.chat(DELETE_CHANNEL) & media_filter)
-async def deltemedia(bot, message):
-    """Delete file from database"""
-    if not message.media:
-        return
-
-    for file_type in ("document", "video", "audio"):
-        media = getattr(message, file_type, None)
-        if media is not None:
-            break
-    else:
-        #await msg.edit('This is not supported file format')
-        return
-
-    file_id, file_ref = unpack_new_file_id(media.file_id)
-
-    result = await Media.collection.delete_one({
-        '_id': file_id,
-    })
-    if result.deleted_count:
-        logger.info('File is successfully deleted from database.')
-    else:
-        file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
-        result = await Media.collection.delete_many({
-            'file_name': file_name,
-            'file_size': media.file_size,
-            'mime_type': media.mime_type
-            })
-        if result.deleted_count:
-            logger.info('File is successfully deleted from database.')
-        else:
-            # files indexed before https://github.com/EvamariaTG/EvaMaria/commit/f3d2a1bcb155faf44178e5d7a685a1b533e714bf#diff-86b613edf1748372103e94cacff3b578b36b698ef9c16817bb98fe9ef22fb669R39 
-            # have original file name.
-            result = await Media.collection.delete_many({
-                'file_name': media.file_name,
-                'file_size': media.file_size,
-                'mime_type': media.mime_type
-            })
-            if result.deleted_count:
-                logger.info('File is successfully deleted from database.')
-            else:
-                logger.info('File not found in database.')
