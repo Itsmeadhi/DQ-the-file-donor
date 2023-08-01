594aba9dc8c58719530ba850f1921b8316b6f39f
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2023-08-01 20:33:26 +0530

    Add port

--- info.py
+++ info.py
@@ -18,6 +18,7 @@ API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
+PORT = environ.get("PORT", "8080")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
